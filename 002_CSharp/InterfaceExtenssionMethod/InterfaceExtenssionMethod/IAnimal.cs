c8a27f068dbc862dcd8cb346c9c3cbb21c87c06f
浩 林山 <adaman3568@gmail.com>
2020-10-30 18:25:46 +0900

    InterfaceExtenssionMethodを追加

---
+++ 002_CSharp/InterfaceExtenssionMethod/InterfaceExtenssionMethod/IAnimal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace InterfaceExtenssionMethod
+{
+    public interface IAnimal
+    {
+        string RoarSound { get; }
+    }
+
+    public class Dog : IAnimal
+    {
+        public string RoarSound => "わんわん";
+    }
+
+    public class Cat : IAnimal
+    {
+        public string RoarSound => "にゃーにゃー";
+    }
+
+    public static class AnimalExtenssionMethod
+    {
+        public static void Roar(this IAnimal animal)
+        {
+            Console.WriteLine(animal.RoarSound);
+        }
+    }
+}
---
+++ 002_CSharp/InterfaceExtenssionMethod/InterfaceExtenssionMethod/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExtenssionMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int num = 2;
+
+            //// 2の2乗
+            //Console.WriteLine(num.Square());
+            //// 2の3乗
+            //Console.WriteLine(num.Square().Square());
+            //// 2の4乗
+            //Console.WriteLine(num.Square().Square().Square());
+
+            //Console.ReadLine();
+
+            //string str = "Hello World";
+            //Console.WriteLine(str.Decoration());
+            //Console.ReadLine();
+
+            var dog = new Dog();
+            var cat = new Cat();
+            dog.Roar();
+            cat.Roar();
+            Console.ReadLine();
+        }
+    }
+
+    public static class IntExtenssionMethod
+    {
+        public static int Square(this int num)
+        {
+            return num * num;
+        }
+    }
+
+    public static class StringExtenssionMethod
+    {
+        public static string Decoration(this string str)
+        {
+            return $@"***{str}***";
+        }
+    }
+}

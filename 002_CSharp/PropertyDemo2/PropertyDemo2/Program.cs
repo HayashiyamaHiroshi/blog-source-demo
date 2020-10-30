f786c81639209bfce4c8860eb7df3cdd191ff298
浩 林山 <adaman3568@gmail.com>
2020-10-30 13:41:15 +0900

    PropertyInfo2を追加

---
+++ 002_CSharp/PropertyDemo2/PropertyDemo2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var user = new User(1, "鈴木", "東京都墨田区");
+            //PropertyInfo[] props = typeof(User).GetProperties();
+            //// ここでAddressのプロパティの情報を抜き出して
+            //var addressProp = props.FirstOrDefault(item => item.Name == nameof(User.Address));
+
+            //// ここで上記プロパティ情報にインスタンスを投げてデータを取得してる。
+            //Console.WriteLine(addressProp.GetValue(user));
+            //Console.ReadLine();
+
+            //PropertyInfo[] props = typeof(User).GetProperties();
+            //var propNames = props.Select(prop => prop.Name);
+            //Console.WriteLine(String.Join(",",propNames.ToArray()));
+            //Console.ReadLine();
+
+            var user = new User(1, "鈴木", "東京都墨田区");
+            PropertyInfo[] props = typeof(User).GetProperties();
+            // ここでAddressのプロパティの情報を抜き出して
+            var addressProp = props.FirstOrDefault(item => item.Name == nameof(User.Address));
+
+            // ここで上記プロパティ情報にインスタンスを投げてデータを取得してる。
+            Console.WriteLine(addressProp.GetValue(user));
+
+            // ここで値を変える
+            addressProp.SetValue(user,"東京都中央区");
+            Console.WriteLine(addressProp.GetValue(user));
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ 002_CSharp/PropertyDemo2/PropertyDemo2/User.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.InteropServices;
+
+namespace PropertyDemo2
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public User(int id, string name, string address)
+        {
+            ID = id;
+            Name = name;
+            Address = address;
+        }
+
+        public User()
+        {
+            
+        }
+    }
+}

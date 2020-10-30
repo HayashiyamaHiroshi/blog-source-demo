9c0d8e80d061eb23d5b26c056d32e601bb07da02
浩 林山 <adaman3568@gmail.com>
2020-10-30 17:15:57 +0900

    PropertyDemo3を追加

---
+++ 002_CSharp/PropertyDemo3/PropertyDemo3/Car.cs
@@ -0,0 +1,21 @@
+﻿namespace PropertyDemo3
+{
+    public class Car
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Maker { get; set; }
+
+        public Car(int id, string name, string maker)
+        {
+            ID = id;
+            Name = name;
+            Maker = maker;
+        }
+
+        public Car()
+        {
+            
+        }
+    }
+}
---
+++ 002_CSharp/PropertyDemo3/PropertyDemo3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyDemo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PropertyInfo[] props = typeof(Car).GetProperties();
+
+            //foreach (PropertyInfo prop in props)
+            //{
+            //    Console.WriteLine($@"{prop.Name} : {prop.PropertyType}");
+            //}
+
+            //var car = new Car(1,"インプレッサ","subaru");
+            //car.Name = "フォレスター";
+
+            var car = new Car(1, "インプレッサ", "subaru");
+            var carNameProp = props.First(item => item.Name == nameof(Car.Name));
+            carNameProp.SetValue(car,"フォレスター");
+            Console.WriteLine(car.Name);
+
+            //foreach (PropertyInfo prop in props)
+            //{
+            //    Console.WriteLine($@"{prop.Name} : {prop.GetValue(car)}");
+            //}
+
+            Console.ReadLine();
+        }
+    }
+}
